29348ee8293ee1364b8904d713ca485201950892
wwilliamwong <wonghinfung0325@gmail.com>
2021-12-23 12:42:22 -0800

    updated login and register page

--- Desktop/cs_project/blog/src/App.js
+++ Desktop/cs_project/blog/src/App.js
@@ -3,13 +3,33 @@ import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
-    <>
+    <Router>
        <TopBar />
-       <Settings />
-    </>
+       <Switch>
+         <Route exact path="/">
+           <Home />
+         </Route>
+         <Route path="/register">
+           {user ? <Home/> : <Register />}
+         </Route>
+         <Route path="/login">
+         {user ? <Home/> : <Login />}
+         </Route>
+         <Route path="/write">
+           <Write />
+         </Route>
+         <Route path="/post/:postId">
+           <Single />
+         </Route>
+       </Switch>
+    </Router>
   );
 }
 
